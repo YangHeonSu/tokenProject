494b3fac10dabd586ac085af908b444f99a95d99
yangheonsu <com6474@gmail.com>
2026-03-30 23:23:45 +0900

    feat : 구글 로그인 API 개발

--- src/main/java/com/dev/project/oauth/OAuthAttributes.java
+++ src/main/java/com/dev/project/oauth/OAuthAttributes.java
@@ -16,11 +16,24 @@ public class OAuthAttributes {
 
     public static OAuthAttributes of(String registerationId, Map<String, Object> attributes) {
 
-        if ("naver".equals(registerationId)) {
+        // 구글 조건 추가
+        if ("google".equals(registerationId)) {
+            return ofGoogle(attributes);
+        } else if ("naver".equals(registerationId)) {
             return ofNaver(attributes);
         } else {
             return ofKakao(attributes);
         }
+    }
+
+    // 구글 생성자 메서드 추가
+    public static OAuthAttributes ofGoogle(Map<String, Object> attributes) {
+        return OAuthAttributes.builder()
+                .loginId("google_" + attributes.get("sub")) // 구글은 식별값 키가 "sub"입니다.
+                .name((String) attributes.get("name"))
+                .email((String) attributes.get("email"))
+                .attributes(attributes)
+                .build();
     }
 
     public static OAuthAttributes ofNaver(Map<String, Object> attributes) {
